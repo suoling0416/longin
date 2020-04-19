e7f37ca416a3a8c82899117be5380ec81cc6c165
suoling <suoll416@163.com>
2020-04-19 08:05:29 +0800

    提交项目

---
+++ src/main/java/com/mr/logingit/LoginGitApplication.java
@@ -0,0 +1,13 @@
+package com.mr.logingit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginGitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoginGitApplication.class, args);
+    }
+
+}
