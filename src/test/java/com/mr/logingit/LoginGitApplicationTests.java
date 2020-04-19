9d455c899963c6d343b8fb069b74f437aa74c452
suoling <suoll416@163.com>
2020-04-19 08:17:15 +0800

    提交全部项目

---
+++ src/test/java/com/mr/logingit/LoginGitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mr.logingit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginGitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
